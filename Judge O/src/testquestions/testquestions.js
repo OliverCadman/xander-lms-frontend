255d923356864cac0b8ed482c8e95a3ac9d87c47
Faran Muhammad <92796969+muhafara@users.noreply.github.com>
2023-02-22 16:41:10 +0000

    Judge O

---
+++ Judge O/src/testquestions/testquestions.js
@@ -0,0 +1,41 @@
+export const testquestions = [
+    {
+        id : 1,
+        excercise_name : 'Addition',
+        excercise_description : 'let variable one is 3 and variable two is 5. Find the sum of two variable which return 8',
+        starter_code : 'def Addition():',
+        expected_output : '8'
+
+    },
+    {
+        id : 2,
+        excercise_name : 'is_Palindrome',
+        excercise_description : 'Write a program to test if the given input is palindrome. It should return true or false',
+        starter_code : 'def isPalindrome():',
+        expected_output : 'true'
+    },
+    {
+        id : 3,
+        excercise_name : 'isEven',
+        excercise_description : 'write a program to test, if number is a even number. It should return true if the number is an even number',
+        starter_code : 'def primeChecker():',
+        expected_output : 'true'
+    },
+    {
+        id : 4,
+        excercise_name : 'Largest Number',
+        excercise_description : 'Given a list, find the largest number. list [21,43,65,6,32,7] should return 65',
+        starter_code : 'def findLargest():',
+        expected_output : '65'
+    },
+    {
+        id : 5,
+        excercise_name : 'Unique name list',
+        excercise_description : 'Implement the unique_names method. When passed two arrays of names, it will return an array containing the names that appear in either or both arrays. The returned array should have no duplicates names1 = ["Ava", "Emma", "Olivia"] names2 = ["Olivia", "Sophia", "Emma"] print(unique_names(names1, names2)) # should print Ava, Emma, Olivia, Sophia',
+        starter_code : 'def unique_name():',
+        expected_output : '[Ava, Emma, Olivia, Sophia]'
+
+    }
+];
+
+
