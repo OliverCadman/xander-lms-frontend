269fca859438af97d5bd3b2f0b3b34b7d3f6ed80
Jack Madden <jackmadden13@hotmail.co.uk>
2023-02-23 10:05:25 +0000

    Saving modules, progress bar, and module button

--- app/src/components/ModuleButton.jsx
+++ app/src/components/ModuleButton.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPlayCircle} from '@fortawesome/free-solid-svg-icons';
@@ -28,35 +27,4 @@ const ModuleButton = ({theme}) => {
   )
 }
 
-=======
-import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPlayCircle} from '@fortawesome/free-solid-svg-icons';
-
-import styled from 'styled-components';
-
-const Button = styled.button`
-    background-color: transparent;
-    color: #202020;
-    padding: 10px 10px;
-    place-items: center;
-    border-radius: 50%;
-    align-items: centre;
-    justify-content: centre;
-    text-transform: uppercase;
-    margin: 40px 50px;
-    font-size: 5rem;
-    cursor: pointer;
-    box-shadow: 0px 2px 2px lightgray;
-    border: none;
-    outline: none;
-    `;
-
-const ModuleButton = ({theme}) => {
-  return (
-    <Button theme={theme}><span><FontAwesomeIcon icon={faPlayCircle}/></span></Button>
-  )
-}
-
->>>>>>> 366853bd67ad535b99d0c76e6f710fbddfdee1d1
 export default ModuleButton
