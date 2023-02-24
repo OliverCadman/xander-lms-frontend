f5a7cead3246c1d1278f6ce609f83c0756d1f834
Jack Madden <jackmadden13@hotmail.co.uk>
2023-02-24 17:22:06 +0000

    Removed duplicated code

--- app/src/components/ModuleWrapper.jsx
+++ app/src/components/ModuleWrapper.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ModuleButton from './ModuleButton';
 import Progress_bar from './Progress_bar';
@@ -16,22 +15,3 @@ const ModuleWrapper = ({header, theme}) => {
 
 export default ModuleWrapper
 
-=======
-import React from 'react';
-import ModuleButton from './ModuleButton';
-import Progress_bar from './Progress_bar';
-
-const ModuleWrapper = ({header, theme}) => {
-  return (
-
-    <div>
-        <h2>{header}</h2>
-        <ModuleButton theme={theme}></ModuleButton>
-        <Progress_bar bgcolor = "#ffffe0" progress = 'Not Started' height = {20} />
-    </div>
-  )
-}
-
-export default ModuleWrapper
-
->>>>>>> 366853bd67ad535b99d0c76e6f710fbddfdee1d1
