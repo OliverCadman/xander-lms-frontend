da8168c49feba4db6d0680cb98faadae53d15a19
Abdul-Aziz Aziz <Abdul.Aziz.Aziz@outlook.com>
2023-03-01 13:29:19 +0000

    New Designs for Topic Page

--- app/src/common/Header.jsx
+++ app/src/common/Header.jsx
@@ -2,17 +2,27 @@ import React, {forwardRef} from 'react';
 
 import styled from 'styled-components';
 
+const StyledH1Wrapper = styled.h1`
+  padding: 3rem;
+`
+
 const StyledH1 = styled.h1`
-    padding: 1rem 0 1rem 0.5rem;
+    padding: 1rem;
+    margin: auto 0;
     font-family: ${props => `${props.fontFamily}, sans-serif`};
     color: ${props => props.color};
     font-weight: 700;
     font-size: 3rem;
+    display: flex;
+    justify-content: center;
+    border: 1px solid black;
 `
 
 const Header = forwardRef((props, ref) => {
   return (
+    <StyledH1Wrapper>
     <StyledH1 ref={ref} fontFamily={props.fontFamily} color={props.color}>{props.content}</StyledH1>
+    </StyledH1Wrapper>
   )
 })
 
